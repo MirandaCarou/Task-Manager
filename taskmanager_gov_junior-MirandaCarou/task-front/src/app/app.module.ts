1432b71a651b4036341d3ea430e62f2356677841
Miranda <71145273+MirandaCarou@users.noreply.github.com>
2023-03-26 15:13:01 +0200

    recontruction basic frontend schema in components

--- taskmanager_gov_junior-MirandaCarou/task-front/src/app/app.module.ts
+++ taskmanager_gov_junior-MirandaCarou/task-front/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { TaskComponent } from './task/task.component';
+import { ToolbarComponent } from './toolbar/toolbar/toolbar.component';
+import { FooterComponent } from './footer/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
+    TaskComponent,
+    ToolbarComponent,
+    FooterComponent
   ],
   imports: [
     HttpClientModule,
